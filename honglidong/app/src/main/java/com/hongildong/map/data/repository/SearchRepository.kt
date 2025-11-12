f68d2d3a82e857ccf508a190f2aa338aea2d3dc5
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2025-11-13 01:14:57 +0900

    feat: 시설 상세정보 api 구현

--- honglidong/app/src/main/java/com/hongildong/map/data/repository/SearchRepository.kt
+++ honglidong/app/src/main/java/com/hongildong/map/data/repository/SearchRepository.kt
@@ -1,10 +1,23 @@
 package com.hongildong.map.data.repository
 
 import com.hongildong.map.data.entity.AutoCompleteSearchKeyword
+import com.hongildong.map.data.entity.FacilityInfo
 import com.hongildong.map.data.entity.NodeInfo
+import com.hongildong.map.data.remote.request.ImageUploadRequest
+import com.hongildong.map.data.remote.request.PhotoRequest
 import com.hongildong.map.data.remote.response.DirectionResponse
+import com.hongildong.map.data.remote.response.ImageUploadResponse
+import com.hongildong.map.data.remote.response.PhotoResponse
 import com.hongildong.map.data.remote.response.RawSearchResponse
+import com.hongildong.map.data.remote.response.ReviewResponse
+import com.hongildong.map.data.util.ApiResponse
 import com.hongildong.map.data.util.DefaultResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchRepository {
     suspend fun searchWithId(accessToken: String, nodeId: Long): DefaultResponse<NodeInfo>
@@ -12,4 +25,33 @@ interface SearchRepository {
     suspend fun autoCompleteSearch(keyword: String): DefaultResponse<List<AutoCompleteSearchKeyword>>
 
     suspend fun direct(from: Int, to: Int): DefaultResponse<DirectionResponse>
+
+    // 시설 정보 검색: type이 facility인 경우
+    suspend fun getFacilityDetail(
+        facilityId: Int
+    ): DefaultResponse<FacilityInfo>
+
+    // 시설 리뷰 조회
+    suspend fun getFacilityReview(
+        accessToken: String, // 리뷰는 회원 기능
+        facilityId: Int,
+        page: Int, // 받아올 페이지 번호. 처음 받아올때는 0으로
+        size: Int // 한번에 받아올 페이지의 크기 (리뷰 개수)
+    ): DefaultResponse<ReviewResponse>
+
+    // 시설 사진 조회
+    suspend fun getFacilityPhoto(
+        accessToken: String, // 사진도 회원 기능
+        facilityId: Int,
+        body: PhotoRequest
+    ): DefaultResponse<PhotoResponse>
+
+    // 시설 리뷰 등록시 사진 등록 - s3 업로드용 링크 받기
+    // s3 사용법
+    // 1. 리뷰에서 사진 등록할 경우 링크 받기 api로 파일명 리스트를 보내서 링크를 받음
+    // 2. 해당 api에서 받은 presignedURL로 Put 요청 보내기
+    // 3. 해당 api에서 받은 imageURL은 리뷰 등록 api로 보내기
+    suspend fun createPresignedUrl(
+        body: ImageUploadRequest
+    ): DefaultResponse<List<ImageUploadResponse>>
 }
--- honglidong/app/src/main/java/com/hongildong/map/data/repository/SearchRepositoryImpl.kt
+++ honglidong/app/src/main/java/com/hongildong/map/data/repository/SearchRepositoryImpl.kt
@@ -1,10 +1,16 @@
 package com.hongildong.map.data.repository
 
 import com.hongildong.map.data.entity.AutoCompleteSearchKeyword
+import com.hongildong.map.data.entity.FacilityInfo
 import com.hongildong.map.data.entity.NodeInfo
 import com.hongildong.map.data.remote.api.SearchService
+import com.hongildong.map.data.remote.request.ImageUploadRequest
+import com.hongildong.map.data.remote.request.PhotoRequest
 import com.hongildong.map.data.remote.response.DirectionResponse
+import com.hongildong.map.data.remote.response.ImageUploadResponse
+import com.hongildong.map.data.remote.response.PhotoResponse
 import com.hongildong.map.data.remote.response.RawSearchResponse
+import com.hongildong.map.data.remote.response.ReviewResponse
 import com.hongildong.map.data.util.DefaultResponse
 import com.hongildong.map.data.util.safeApiCall
 import javax.inject.Inject
@@ -36,5 +42,30 @@ class SearchRepositoryImpl @Inject constructor(
         to: Int
     ): DefaultResponse<DirectionResponse> {
         return safeApiCall { api.direct(from, to) }
+    }
+
+    override suspend fun getFacilityDetail(facilityId: Int): DefaultResponse<FacilityInfo> {
+        return safeApiCall { api.getFacilityDetail(facilityId) }
+    }
+
+    override suspend fun getFacilityReview(
+        accessToken: String,
+        facilityId: Int,
+        page: Int,
+        size: Int
+    ): DefaultResponse<ReviewResponse> {
+        return safeApiCall { api.getFacilityReview(accessToken, facilityId, page, size) }
+    }
+
+    override suspend fun getFacilityPhoto(
+        accessToken: String,
+        facilityId: Int,
+        body: PhotoRequest
+    ): DefaultResponse<PhotoResponse> {
+        return safeApiCall { api.getFacilityPhoto(accessToken, facilityId, body) }
+    }
+
+    override suspend fun createPresignedUrl(body: ImageUploadRequest): DefaultResponse<List<ImageUploadResponse>> {
+        return safeApiCall { api.createPresignedUrl(body) }
     }
 }
