18c32fc03ec372a14d95573d9b7538b0be934fee
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2025-11-23 23:13:49 +0900

    fix: 시간 파싱 수정

--- honglidong/app/src/main/java/com/hongildong/map/ui/util/TimeFormatter.kt
+++ honglidong/app/src/main/java/com/hongildong/map/ui/util/TimeFormatter.kt
@@ -1,16 +1,16 @@
 package com.hongildong.map.ui.util
 
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
-// 컬렉션 생성/수정 날짜 데이터 포매팅 yyyy.MM.dd 형태
+// 생성/수정 날짜 데이터 포매팅 yyyy.MM.dd 형태
 fun formatDate(isoString: String): String {
     return try {
-        val inputFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
-        val date = LocalDate.parse(isoString, inputFormatter)
+        val parsedDate = LocalDateTime.parse(isoString)
         val outputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-        outputFormatter.format(date)
+        outputFormatter.format(parsedDate)
     } catch (e: DateTimeParseException) {
         isoString
     }
